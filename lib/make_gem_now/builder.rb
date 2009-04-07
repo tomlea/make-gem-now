fb16460491bbd378080fdad7d145dd9cb9bee37c
Tom Lea <commit@tomlea.co.uk>
2009-04-07 18:51:11 +0100

    Add the ability to detect if a gem has already been built.

--- lib/make_gem_now/builder.rb
+++ lib/make_gem_now/builder.rb
@@ -10,34 +10,42 @@ module MakeGemNow
       @output_path = File.expand_path(output_path)
       raise "Could not find gemspec!" unless File.exist? @path_to_gemspec
     end
-    
+
     def generate_spec
       spec_file = File.read(@path_to_gemspec)
 
       IO.popen("-"){|yaml_pipe|
         if yaml_pipe
           Timeout.timeout(1){
-            @spec = YAML.load(yaml_pipe)
-            raise "This isn't a freaking gemspec, what you trying to pull here buddy?" unless @spec.class == Gem::Specification
+            spec = YAML.load(yaml_pipe)
+            raise "This isn't a freaking gemspec, what you trying to pull here buddy?" unless spec.class == Gem::Specification
+            return spec
           }
         else
           puts eval("$SAFE = 3\n (\n#{spec_file}\n).to_yaml")
         end
       }
-      
+
       raise "Loading the YAML failed." unless $?.success?
     end
-    
+
+    def spec
+      @spec ||= generate_spec
+    end
+
+    def should_build?
+      ! File.exists?(File.join(@output_path, spec.file_name))
+    end
+
     def build!
       old_pwd = Dir.pwd
-      generate_spec
       Dir.chdir(File.dirname(@path_to_gemspec))
-      open File.join(@output_path, @spec.file_name), 'wb' do |gem_io|
+      open File.join(@output_path, spec.file_name), 'wb' do |gem_io|
         Gem::Package.open gem_io, 'w', nil do |pkg|
-          pkg.metadata = @spec.to_yaml
-          @spec.files.each do |file|
+          pkg.metadata = spec.to_yaml
+          spec.files.each do |file|
             next if File.directory? file
-            next if file == @spec.file_name # Don't add gem onto itself
+            next if file == spec.file_name # Don't add gem onto itself
 
             stat = File.stat file
             mode = stat.mode & 0777
